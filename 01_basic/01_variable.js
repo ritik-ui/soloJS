74df28e6f63ce2b036f502c674830dd3f676b04c
ritik-ui <82826667+ritik-ui@users.noreply.github.com>
2024-06-11 05:07:00 +0000

    variables

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,9 @@
+const accountId = 1234;
+
+let accountEmail = "ritiksingh@gmail.com"
+
+var accountPass = "121"
+ accountLoc = "mumbai";
+
+accountEmail ="rritik@gmail.com"
+ console.table([accountId,accountPass,accountEmail,accountLoc])
