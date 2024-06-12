07e13b034db4374d475c1f35b6c38b54a21bcf5a
ritik-ui <82826667+ritik-ui@users.noreply.github.com>
2024-06-12 05:12:10 +0000

    console.log(typeof  )

---
+++ 01_basic/datatype.js
@@ -0,0 +1,38 @@
+//primitive
+
+// 7 types: - string,number,boolean,null, undefined,BigInt,symbol
+
+// let name="Ritik";
+
+// let score = 23;
+
+// let isLoggedIn = false
+
+// let afterScore = null;
+
+// let address;
+
+// let highScore = 21343454565n;
+
+
+// let symName = Symbol('123');
+
+
+
+//Non Premative
+
+
+// Array,object, function
+
+const hero = ["wizard","archer","royal","grandwarder"];
+
+let myObj = {
+    name:"Ritik",
+    age : 22,
+}
+
+
+const heero = function(){
+    console.log("hey super")
+}
+console.log(typeof  heero);
